32d047ce70e75555937bd8da2e93f8d667a09299
Guillermo Acosta <guillermo.acosta@tarmac.io>
2020-03-25 00:58:18 -0300

    chore: add REDUX_DEVTOOLS_EXTENSION reducer & debug script

--- App.js
+++ App.js
@@ -12,7 +12,10 @@ enableScreens();
 const rootReducer = combineReducers({
   meals: mealsReducer
 });
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default class App extends React.Component {
   state = {
